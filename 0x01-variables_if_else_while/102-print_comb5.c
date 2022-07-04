546a743da3504e7fc93262ddb41937da792ab8b8
Bamise Adebobola <bamiseadebobola@outlook.com>
2022-07-04 15:19:32 -0700

    fixing code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,8 +5,8 @@
  * main - Prints all possible combinations of two two-digit numbers,
  * Return: Always 0.
  */
-int main(void);
-^{
+int main(void)
+{
 	int num1, num2;
 
 	for (num1 = 0; num1 <= 98; num1++)
