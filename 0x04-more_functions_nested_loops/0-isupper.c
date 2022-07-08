b7e73c894c9c5410fd5cb6ffced181c69d17bb08
Bamise Adebobola <bamiseadebobola@outlook.com>
2022-07-08 07:40:55 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,7 @@
  * main - Entry point
  * _isupper - To check if the subject is uppercase or lowercase
  * @c: Parameter for subject
- * Return: ALways 0.
+ * Return: Always 0.
  */
 int main(void)
 {
