a1e75d0f164d5e48854bd9a591d4d77ed2d316b7
Bamise Adebobola <bamiseadebobola@outlook.com>
2022-07-08 07:32:07 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,19 @@
 # include <stdio.h>
 # include "main.h"
 /**
- * _isupper: To check if the subject is uppercase or lowercase
+ * main - Entry point
+ * _isupper - To check if the subject is uppercase or lowercase
  * @c: Parameter for subject
  * Return: ALways 0.
  */
+int main(void)
+{
+	return (0);
+}
+
 int _isupper(int c)
 {
-	if(c = 'A' && c <= 'Z')
+	if (c = 'A' && c <= 'Z')
 		return (0);
 	else
 		return (1);
