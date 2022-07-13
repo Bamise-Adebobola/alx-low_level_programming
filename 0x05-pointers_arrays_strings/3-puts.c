6d5c650ec5e57454661aa015b18754c517613afa
Bamise Adebobola <bamiseadebobola@outlook.com>
2022-07-13 11:09:21 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * _puts - prints a string to the screen
+ * @str: pointer to the string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
