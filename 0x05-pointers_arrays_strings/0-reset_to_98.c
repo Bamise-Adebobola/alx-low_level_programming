ae585e919aa63be623db02979199b4ae6f6f3256
Bamise Adebobola <bamiseadebobola@outlook.com>
2022-07-13 08:58:56 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,14 @@
 # include <stdio.h>
 # include "main.h"
 
+/**
+ * reset_to_98 - this code resets the value of n to 98.
+ * @n: Parameter for change
+ * Return: void
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
