60c2f0bd3d32c41881c28c6d8b9ca84c08aaa551
Bamise Adebobola <bamiseadebobola@outlook.com>
2022-07-13 12:46:15 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include "main.h"
+/**
+ * print_rev - Reverses strings
+ * @s: Pointer for variable
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	printf("%s\n", strrev(s));
+}
+
