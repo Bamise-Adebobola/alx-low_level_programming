c4f7e1731a32e3c112157d6f2ce05b7252b61757
Bamise Adebobola <bamiseadebobola@outlook.com>
2022-07-13 10:35:29 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * _strlen - This function prints how many characters are in a string.
+ * @s: Parameter for char
+ * Return: void.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	int i = 0;
+
+	for (i = 0; i < strlen(s); i++)
+		count++;
+	return (count);
+}
+
